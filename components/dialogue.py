7eea8b04a0eda9248bcc6ddcad54b0e30f6b780d
GT-77 <mister.gt771@gmail.com>
2019-04-10 17:48:40 +0300

    gettin started 4 real part 2

--- components/dialogue.py
+++ components/dialogue.py
@@ -1,24 +1,79 @@
 import utilities as u
-def _replysituation(situation):
-    # returns a dictionary with its keys being all txt file names in dialogue/'situation' without the .txt
-    # and its values being tuples
-    # with their first values being the replies contained in that txt file
-    # excluding the first line, which will contain a + or something else
-    # if it contains a plus, the replies will include those of DEFAULT.txt
+def _adddict(dict0, dict1):
+    # adds dictionary dict1 into dict0
+    for key in dict1:
+        if key not in dict0:
+            dict0[key] = dict1[key]
+        else:
+            dict0[key] += dict1[key]
+# get ready for the biggest inception of functions ever
+def _stringtoreplies(string, default):
+    # turns given 'string' (what you see in dialogue.txt files) into a list of replies to actually use, relative to given 'default'
+    # the format of what you see in dialogue.txt files is the following:
+    #   [+ or -]
+    #   [possible reply 1]
+    #   [possible reply 2]
+    #   .
+    #   .
+    #   .
+    #   [last possible reply]
+    # this function will just split everything bellow the + or - into a list
+    # if there's a + at the top, this function will also add to the list the replies given in the 'default' parameter, else it will add nothing
+    # really i just add - because i want to. any character except + would do for a valid dialogue file
+    return list(filter(bool, string[2:].split("\n") + (default if string[0] == "+" else [])))
+def _fileaddresstoreplies(fileaddress, default):
+    # parses the file of the adress 'fileaddress' into a list of replies to use, relative to given 'default'
+    # the function _stringtoreplies only handles the contents of the file. this one is the real deal
+    return _stringtoreplies(u.quickread(fileaddress), default)
+def _addrepliestodictionary(dictionary, fileaddress, default):
+    # sets the key with the name being 'fileaddress' without the '.txt' and address fuss to the parsed contants of the file relative to given 'default'
+    # this function handles converting the address to a just the raw filename for the key
+    dictionary.update({fileaddress[:-4].split("/")[-1]: _fileaddresstoreplies(fileaddress, default)})
+def _dictionaryofreplies(folderaddress):
+    # parses given folder of txt files with the address 'folderaddress' to a dictionary with its keys the parsed names of the txt files and values the parsed replies of the corresponding files
     returning = dict()
-    returning["DEFAULT"] = u.quickread("dialogue/command/{}/{}".format(situation, "DEFAULT.txt"))[2:].split("\n") # setting this one separately to avoid key errors (it's not guaranteed that the loop will start with "DEFAULT.txt")
-    for commandname in u.filesindir("dialogue/command/{}".format(situation)):
-        content = u.quickread("dialogue/command/{}/{}".format(situation, commandname))
-        returning[commandname[:-4]] = content[2:].split("\n") + (content["DEFAULT"] if content[0] == "+" else [])
+    _addrepliestodictionary(returning, "{}/{}".format(folderaddress, "DEFAULT.txt"), [])
+    for fileaddress in u.filesindir(folderaddress):
+        _addrepliestodictionary(returning, "{}/{}".format(folderaddress, fileaddress), returning["DEFAULT"])
     return returning
-commandreplies = dict( # dictionary containing all possible situations of a command
-    success = _replysituation("success"),
-    fail = _replysituation("fail"),
-    invalid = _replysituation("invalid"),
-)
+def _dictionaryofdictionariesofreplies(folderaddress):
+    # parses given folder of folders of txt files to a dictionary of dictionaries of replies
+    # a folder of the folder has its naming '[+ or -] [name]', if it has a + it inherits from the '- DEFAULT' folder. you get the gist
+    returning = dict()
+    returning["DEFAULT"] = _dictionaryofreplies("{}/{}".format(folderaddress, "- DEFAULT"))
+    for subfoldername in u.foldersindir(folderaddress):
+        returning[subfoldername[2:]] = _dictionaryofreplies("{}/{}".format(folderaddress, subfoldername))
+        if subfoldername[0] == "+":
+            _adddict(returning[subfoldername[2:]], returning["DEFAULT"])
+    return returning
+
+commandreplies = dict() # dictionary dctionaries of replies for every given command situation
+for situation in ("success", "fail", "invalid"):
+    commandreplies[situation] = _dictionaryofreplies("dialogue/command/{}".format(situation))
 def commandreply(commandname, situation):
-    # returns a string of a reply sir gts can give when the command with the name 'commandname' has succeeded
-    if commandname in commandreplies[situation]:
-        return u.randomelement(commandreplies[situation][commandname])
+    # returns a string of a reply sir gts can give when the command with the name 'commandname' gives out situation 'situation'
+    if commandname not in commandreplies[situation]:
+        commandname = "DEFAULT"
+    return u.randomelement(commandreplies[situation][commandname])
+
+pingreplies = _dictionaryofreplies("dialogue/ping") # dictionary with its keys being user ids and values being subscriptables containing possible replies to those users
+def pingreply(userid):
+    # returns a string of a reply gts can give when he gets pinged, depending by who (userid)
+    if userid not in pingreplies:
+        userid = "DEFAULT"
+    return u.randomelement(pingreplies[userid])
+
+passivereplies = _dictionaryofdictionariesofreplies("dialogue/passive")
+def passivereply(messagecontent, userid):
+    if userid not in passivereplies:
+        userid = "DEFAULT"
+    for line in messagecontent.split("\n"):
+        for word in line.split(" "):
+            if word.lower() in passivereplies[userid]:
+                break
+        else:
+            continue
+        break
     else:
-        return u.randomelement(commandreplies[situation]["DEFAULT"])
+        word = "DEFAULT"
+    return u.randomelement(passivereplies[userid][word])
--- components/test.py
+++ components/test.py
@@ -1,6 +1,9 @@
 import dialogue as d
-print(d.commandreplies)
+print(d.passivereplies)
 x = input()
 while x != "exit":
-    print(d.u.randomelement(['s', 'xyz', 'kek', '2b2t']))
+    x = x.split("| ")
+    print(x)
+    for i in range(50):
+        print("<G!T!S><> ", d.passivereply(x[0], x[1]))
     x = input()
