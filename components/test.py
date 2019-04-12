432f98e7df43f41b98efa0e3caa1912229e360f0
GT-77 <mister.gt771@gmail.com>
2019-04-12 11:55:05 +0300

    two two one

--- components/test.py
+++
@@ -1,9 +0,0 @@
-import dialogue as d
-from pprint import pprint
-pprint(d.passivereplies)
-x = input()
-while x != "exit":
-    x = x.split("| ")
-    for i in range(50):
-        print(d.passivereply(x[0], x[1]))
-    x = input()
