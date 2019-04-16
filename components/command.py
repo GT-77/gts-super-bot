e49cee4b9946170c5f549e5fc8d733a2d14bf9b7
GT-77 <mister.gt771@gmail.com>
2019-04-16 10:13:53 +0300

    1 small step 4 man 1 big sep 4 lil marco & friends

--- components/command.py
+++ components/command.py
@@ -3,57 +3,53 @@ from collections import OrderedDict # gon need this later
 PREFIX = "7!" # this is the prefix gts responds to. yes, if you change this you indeed change everything (!!! prefixes and command names should not contain spaces !!!)
 NO_DEFAULT = type("NO_DEFAULT", (), dict()) # i use this to mark whenever a parameter has no default value, thus 'None' is a default value and so is '"DEFAULT"', but 'command.NO_DEFAULT' isn't
 NO_COMPILING = lambda x: x # function that does no compiling whatsoever and leaves the argument as it is in the message
-def _convert(parameter):
-    # converts a parameter given as an argument when creating a 'Command' to the normal format of a parameter
-    # format of a parameter is the following: (parameter_name, default_value, compiler_function)
 
-    # parameter_name represents the key you will give the 'arg' 'OrderedDict' (or whatever else you called it) inside your command code to access the value
-    # default_value represents the value given to 'arg' when the discord message has not specified the value. if you set it to NO_DEFAULT then gts will not treat that argument as optional and will be unhappy when you don't give it a value in your discord message command
-    # compiler_function represents a function which takes given value as an argument in string format and returns the useful format. if it returns NO_DEFAULT the input is considered invalid and gts flips the bird
+class Parameter:
+    def __init__(self, name, default_value = NO_DEFAULT, compiler = NO_COMPILING):
+        self.name = name
+        self.default_value = default_value
+        self.compile = compiler
+    def kvpair(self, argument = None):
+        if argument is None:
+            argument = self.default_value
+        return {self.name: self.compile(argument)}
+    def is_necessary(self):
+        return self.default_value is NO_DEFAULT
+    def is_optional(self):
+        return not self.is_necessary()
 
-    # examples:
-    #   parameter = "xyz"
-    #   then return ("xyz", NO_DEFAULT, NO_COMPILING)
-
-    #   parameter = ("amount", "5")
-    #   then return ("amount", "5", NO_COMPILING)
-    if isinstance(parameter, tuple):
-        if len(parameter) < 3:
-            parameter += NO_COMPILING,
-    else:
-        parameter = parameter, NO_DEFAULT, NO_COMPILING
-    return parameter
-
-def _write_over_ordered_dict(ordered_dict, lis):
-    # updates the 'ordered_dict' 's values by iterating the list 'lis' over them
-    # 'lis' gets consumed
-    returning = ordered_dict
-    for key in returning:
-        if lis:
-            returning[key] = lis.pop(0)
-        else:
-            break
-    return returning
+class Parameters:
+    def __init__(self, *parameters):
+        self.parameters = tuple((Parameter(*(parameter if isinstance(parameter, tuple) else (parameter,))) for parameter in parameters))
+        self.necessary_parameters = sum(parameter.is_necessary() for parameter in self.parameters)
+    def arg(self, message_content):
+        cm = message_content.split(" ")[1:]
+        len_cm = len(cm)
+        if len_cm < self.necessary_parameters:
+            return None
+        for i in range(len(self.parameters) - len_cm):
+            cm.append(None)
+        arg_ = dict()
+        for parameter, argument in zip(self.parameters, cm):
+            arg_.update(parameter.kvpair(argument))
+        return arg_
 
 class Command:
     def __init__(self, func, command_name, *parameters, **options):
         # a Command object needs to be initialized like this:
         #   Command(function, command_name, parameter0, parameter1, ... , option0, option1, ... )
         # !!! parameters with NO_DEFAULT must be given first !!!
-        self._func = func
+        self.func = func
         self.name = command_name
-        self._defaults, self._compilers = multipleinstances(OrderedDict, 2)
-        for parameter in map(_convert, parameters):
-            self._defaults[parameter[0]], self._compilers[parameter[0]] = parameter[1], parameter[2]
-    async def run(self, messagecontent):
+        self.parameters = Parameters(*parameters)
+    async def run(self, message_content):
         # executes command for given message content
-        # (this ignores whether the command in the call is the same as this one and just reads the parameters)
-        arg = _write_over_ordered_dict(self._defaults, messagecontent.split(" ")[1:]) # write input from the message over default values
-        arg = OrderedDict(((parameter, self._compilers[parameter](arg[parameter])) for parameter in arg)) # run validators over values
-        if NO_DEFAULT in arg.values():
+        # (this ignores whether the command in the call is the same as this one and just reads the arguments)
+        arg = self.parameters.arg(message_content)
+        if arg is None:
             return "invalid"
         try:
-            await self._func(arg)
+            await self.func(arg)
         except:
             return "fail"
         return "success"
